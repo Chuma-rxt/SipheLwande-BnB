c7d6b919decef053bbd44a88044a4af5814c4499
Chuma Raxothi <141155118+Chuma-rxt@users.noreply.github.com>
2024-04-25 15:14:05 +0200

    Images

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-// var menulist = document.getElementById('menulist');
-// menulist.style.maxHeight = "0px";
+var menulist = document.getElementById('menulist');
+menulist.style.maxHeight = "0px";
 
-// function menutoggle() {
-//     if (menulist.style.maxHeight == "0px") {
-//         menulist.style.maxHeight = "100vh";
-//     } else {
-//         menulist.style.maxHeight = "0px";
-//     }
-// }
+function menutoggle() {
+    if (menulist.style.maxHeight == "0px") {
+        menulist.style.maxHeight = "100vh";
+    } else {
+        menulist.style.maxHeight = "0px";
+    }
+}
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
